a26d5bc7916c3c4ab4012f643464fddb88bfc6c9
egesenkul <egesenkul35@hotmail.com>
2018-04-03 12:40:00 +0300

    Yeni logo eklenmiştir

--- lib/anaEkran.dart
+++ lib/anaEkran.dart
@@ -1,2 +1,21 @@
 import 'package:flutter/material.dart';
 
+class anaEkran extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text('Home'),),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              new Text('Ana ekran'),
+              new RaisedButton(onPressed: (){Navigator.of(context).pushNamed('/Second');})
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/thirdpage.dart
+++ lib/thirdpage.dart
@@ -12,64 +12,80 @@ class _ThirdDartPAGEState extends State<ThirdDartPAGE> {
   @override
   Widget build(BuildContext context) {
 
-    var _firstGroup = new Container(
-      decoration: new BoxDecoration(
-        border: new Border.all(
-          color: Colors.white,
-          width: 2.0,
-        ),
-      ),
-      child: new Padding(
-        padding: new EdgeInsets.all(20.0),
-        child: new Column(
-          children: <Widget>[
-            new Text("First Group"),
-            new RaisedButton(
-              child: new Text("First Button"),
-              onPressed: _onPress,
-            ),
-          ],
-        ),
-      ),
-    );
-
-    var _secondGroup = new Container(
-      decoration: new BoxDecoration(
-        border: new Border.all(
-          color: Colors.white,
-          width: 2.0,
-        ),
-      ),
-      child: new Padding(
-        padding: new EdgeInsets.all(20.0),
-        child: new Column(
-          children: <Widget>[
-            new Text("Second Group"),
-            new RaisedButton(
-              child: new Text("Second Button"),
-              onPressed: _onPress,
-            ),
-          ],
-        ),
-      ),
-    );
-
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Giriş Sayfası"),
+        title: new Text("Haberler"),
       ),
-      body: new Container(
-        child: new Column(
+      drawer: new Drawer(
+        child: new ListView(
           children: <Widget>[
-            _firstGroup,
-            _secondGroup,
+            new UserAccountsDrawerHeader(
+              accountName: new Text("Ege Şenkul",style: new TextStyle(fontWeight: FontWeight.bold),),
+              accountEmail: new Text("egesenkul35@hotmail.com"),
+              currentAccountPicture: new GestureDetector(
+                onTap: null,
+                child: new CircleAvatar(
+                  backgroundImage: new NetworkImage("https://scontent-otp1-1.xx.fbcdn.net/v/t1.0-9/20729397_10155281919170617_5321691683692301674_n.jpg?_nc_cat=0&oh=c512bdc66bd2f6bf0c767d88b2573175&oe=5B2F10F0"),
+                ),
+              ),
+              decoration: new BoxDecoration(
+                image: new DecorationImage(
+                  fit:BoxFit.fill,
+                    image: new NetworkImage("https://bilgibirikimi.net/wp-content/uploads/2012/04/izmir-saat-kulesi.jpg"))
+              ),
+            ),
+            new ListTile(
+              title:  new Text("Yeni Haber"),
+              trailing: new Icon(Icons.add),
+            ),
+            new Divider(),
+            new ListTile(
+              title:  new Text("Tümü"),
+              trailing: new Icon(Icons.assignment),
+            ),
+            new ListTile(
+              title:  new Text("Radarlar ve Çevirmeler"),
+              trailing: new Icon(Icons.add_alert),
+            ),
+            new ListTile(
+              title:  new Text("Kazalar"),
+              trailing: new Icon(Icons.block),
+            ),
+            new ListTile(
+              title:  new Text("Yol Bilgisi"),
+              trailing: new Icon(Icons.info),
+            ),
+            new ListTile(
+              title:  new Text("Çekiciler"),
+              trailing: new Icon(Icons.drive_eta),
+            ),
+            new ListTile(
+              title:  new Text("Kayıp Eşyalar"),
+              trailing: new Icon(Icons.feedback),
+            ),
+            new ListTile(
+              title:  new Text("Duyurular"),
+              trailing: new Icon(Icons.error),
+            ),
+            new Divider(),
+            new ListTile(
+              title:  new Text("Onaylanmamış Gönderiler"),
+              trailing: new Icon(Icons.touch_app),
+              onTap: ()=>  Navigator.of(context).pop(),
+            ),
+            new Divider(),
+            new ListTile(
+              title:  new Text("Ayarlar"),
+              trailing: new Icon(Icons.settings),
+              onTap: ()=>  Navigator.of(context).pop(),
+            ),
+            new ListTile(
+              title:  new Text("Çıkış"),
+              trailing: new Icon(Icons.power_settings_new),
+              onTap: ()=>  Navigator.of(context).pop(),
+            ),
           ],
         ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        child: new Icon(Icons.add),
-        tooltip: 'Add',
-        onPressed: null,
       ),
     );
   }
