ab9381ad9fefe1711d3e8b63687ccd2993b8f887
Rıza Özonuk <riza.gs1905@hotmail.com>
2018-04-05 13:26:40 +0200

    Image Picker Added

--- lib/newNewsPage.dart
+++ lib/newNewsPage.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class NewNewsPage extends StatefulWidget {
   @override
@@ -6,11 +9,31 @@ class NewNewsPage extends StatefulWidget {
 }
 
 class _NewNewsPageState extends State<NewNewsPage> {
+  File _image;
+
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _image = image;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Haber Oluştur"),
+        title: new Text('Haber Oluştur'),
+      ),
+      body: new Center(
+        child: _image == null
+            ? new Text('No image selected.')
+            : new Image.file(_image),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: new Icon(Icons.add_a_photo),
       ),
     );
   }
