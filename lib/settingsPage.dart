746276d9c7c873154c752b3532633185cd503961
onat35 <onat.gs1995@hotmail.com>
2018-04-27 12:39:57 +0300

    Hakkında sayfası tamamlandı.

--- lib/settingsPage.dart
+++ lib/settingsPage.dart
@@ -1,20 +1,59 @@
 import 'package:flutter/material.dart';
 
-class SettingsPage extends StatefulWidget {
-  @override
-  _SettingsPageState createState() => new _SettingsPageState();
-}
+class SettingsPage extends StatelessWidget {
+  static String tag = 'Hakkımızda';
 
-class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Ayarlar"),
+    final ege = new Hero(
+      tag: 'hero',
+      child: new Padding(
+        padding: EdgeInsets.all(16.0),
+        child:  new CircleAvatar(
+          radius: 72.0,
+          backgroundColor: Colors.transparent,
+          backgroundImage: AssetImage('assets/ege.jpg'),
+        ),
       ),
-      body: new Container(
-        child: new Text('ege')
+    );
+
+    final merhaba = new Padding(
+      padding: EdgeInsets.all(8.0),
+      child: new Text(
+        'İzmir Trafik Çevirmeye Hoşgeldiniz',
+        style: new TextStyle(fontSize: 28.0, color: Colors.white),
       ),
+    );
+
+    final hakkinda= new Padding(
+      padding: EdgeInsets.all(8.0),
+      child: new Text(
+        'Prograrımıza hoşgeldiniz. Bu program sizlere trafikteyken diğer insanlarala iletişim içinde kalmanız amacıyka yaratılmıştır',
+        style: new TextStyle(fontSize: 16.0, color: Colors.white),
+      ),
+    );
+    final not = new Padding(
+      padding: EdgeInsets.all(8.0),
+      child:  new Text('Not: Bu programın kötüye kullanılması uyarısız ban sebebidir.'),
+    );
+
+
+    final body = new Container(
+      width: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.all(28.0),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Colors.blue,
+          Colors.lightBlueAccent,
+        ]),
+      ),
+      child: new Column(
+        children: <Widget>[ege, merhaba, hakkinda,not]
+      ),
+    );
+
+    return Scaffold(
+      body: body,
     );
   }
 }
